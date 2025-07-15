8e3c048ef7377a5eb99149341abd17d186f2da79
Maharshii <93990657+Nobodyyisme@users.noreply.github.com>
2025-07-15 20:53:14 +0530

    Update config.py

--- backend/config.py
+++ backend/config.py
@@ -3,9 +3,10 @@ import os
 
 
 class Config:
-    MYSQL_HOST = 'localhost'
+    MYSQL_HOST = '0.tcp.in.ngrok.io'
+    MYSQL_PORT = 18473  # This is important to add
     MYSQL_USER = 'root'
-    MYSQL_PASSWORD = ''
+    MYSQL_PASSWORD = ''  # Add your local MySQL password if set
     MYSQL_DB = 'image_attendance'
     SECRET_KEY = 'super-secret-key'
-    JWT_SECRET_KEY = os.getenv("JWT_SECRET_KEY")
+    JWT_SECRET_KEY = os.getenv("bbbvvv38c8c26e13caa42d88a54b294ad47e3c27ee787170731e0fc21a45bb61")
