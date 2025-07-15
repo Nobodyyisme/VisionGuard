d4842f06818f718d83678f18475656e757ce3ddf
Maharshii <93990657+Nobodyyisme@users.noreply.github.com>
2025-07-15 21:12:32 +0530

    Update config.py

--- backend/config.py
+++ backend/config.py
@@ -4,9 +4,9 @@ import os
 
 class Config:
     MYSQL_HOST = '0.tcp.in.ngrok.io'
-    MYSQL_PORT = 18473  # This is important to add
-    MYSQL_USER = 'root'
-    MYSQL_PASSWORD = ''  # Add your local MySQL password if set
+    MYSQL_PORT = 12786                      # Updated Ngrok port
+    MYSQL_USER = 'root'                     # Default XAMPP user
+    MYSQL_PASSWORD = ''                     # Use your MySQL password here if you set one
     MYSQL_DB = 'image_attendance'
     SECRET_KEY = 'super-secret-key'
     JWT_SECRET_KEY = os.getenv("bbbvvv38c8c26e13caa42d88a54b294ad47e3c27ee787170731e0fc21a45bb61")
